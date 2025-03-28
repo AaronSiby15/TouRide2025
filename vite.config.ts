bd84644c7a340ba4fbe3445def391508f1a3c9a4
AaronSiby15 <asiby2@uwo.ca>
2025-03-28 17:32:40 -0400

    C:/Program Files/Git/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TouRide2025",
+  base: "/TouRide2025/",
   optimizeDeps: {
 
     exclude: ['lucide-react'],
