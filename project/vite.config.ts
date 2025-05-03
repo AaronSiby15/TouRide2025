92ca750c24f921bab02e1cb41f7511e99b7dc2e2
AaronSiby15 <asiby2@uwo.ca>
2025-05-02 21:37:43 -0400

    browser icon + visual blur for payment

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // Unless the route becomes http://localhost:5173/TouRide2025/
   // base: "/TouRide2025/",
+  
   optimizeDeps: {
 
     exclude: ['lucide-react'],
